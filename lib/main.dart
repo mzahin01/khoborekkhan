efbe6d21cd58071ef66b6a49ef6ab9bc9613b5e4
mzahin01 <minhazahin@gmail.com>
2025-06-29 17:10:22 +0600

    adding dep

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Hello, world!')),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello, world!'))),
     );
   }
 }
